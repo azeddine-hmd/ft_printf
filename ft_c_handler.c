c3b14c2379a0efc2a0c2bbdc477173fb512f21f9
Azeddine Hamdaoui <ahamdaou@e2r5p7.1337.ma>
2019-11-28 02:14:36 +0100

    updating

--- ft_c_handler.c
+++ ft_c_handler.c
@@ -1,11 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_c_handler.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahamdaou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/27 23:53:04 by ahamdaou          #+#    #+#             */
+/*   Updated: 2019/11/27 23:58:34 by ahamdaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
 int		ft_c_handler(va_list arglst, t_lilst *data)
 {
 	char	arg;
+	char	*flag;
+	char	*width
 
 	arg = va_arg(arglst, int);
-	data->flags = ft_strdup("flags");
 	printf("ft_c_handler reached\n");
+	ft_split
 	return (1);
 }
