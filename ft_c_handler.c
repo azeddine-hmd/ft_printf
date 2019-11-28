c4d5d81f4e75b0f646b685d20fcb10f186d35b3a
azeddine-hmd <azeddine.hmd@gmail.com>
2019-11-28 14:34:51 +0100

    fast update

--- ft_c_handler.c
+++ ft_c_handler.c
@@ -15,11 +15,8 @@
 int		ft_c_handler(va_list arglst, t_lilst *data)
 {
 	char	arg;
-	char	*flag;
-	char	*width
 
 	arg = va_arg(arglst, int);
 	printf("ft_c_handler reached\n");
-	ft_split
 	return (1);
 }
